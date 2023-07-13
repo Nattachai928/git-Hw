f3c03a06245d7303556286a7302cd18593707f5d
Nattachai928 <138751701+Nattachai928@users.noreply.github.com>
2023-07-13 16:30:20 +0700

    frist commit

---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php echo "MR.NATTACHAI SROISUNGANE"; ?>
+<?php echo "STUDENTNUMBER : 6411130017"; ?>
+<?php echo "MAJOR : ENGINEERING COMPUTER "; ?>
+<?php echo "TEL : 888888888888"; ?>
+<?php echo "I LIKE CAT "; ?>
